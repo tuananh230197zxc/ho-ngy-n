ef4deb083114f2f972a2be5ed17aff365bdb07ef
tuananh230197zxc <88013981+tuananh230197zxc@users.noreply.github.com>
2023-03-28 22:52:37 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = 'https://www.facebook.com/profile.php?id=100011356655214';
+const eventName = 'Chúc mừng sinh nhật';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Yến sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
